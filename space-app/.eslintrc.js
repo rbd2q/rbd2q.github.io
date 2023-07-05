ace4204fbd17d29cb025c24542557048866b6095
vaniasuveev <vania-suveev@mail.ru>
2023-07-06 01:12:53 +0300

    chore(deps): configured eslint and prettier

---
+++ space-app/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  "root": true,
+  plugins: ['prettier', 'simple-import-sort', "react", "react-hooks", "jsx-a11y"],
+  extends: [
+    "eslint:recommended",
+    'airbnb',
+    'prettier',
+  ],
+  rules: {
+    "sort-imports": "off",
+    "import/order": "off",
+    "simple-import-sort/imports": "error",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [1, {"extensions": [".js", ".jsx"]}],
+    "import/prefer-default-export": "off",
+    "react/function-component-definition": [2, {"namedComponents": "arrow-function"}],
+    "import/no-extraneous-dependencies": ["error", {
+      "devDependencies": false,
+      "optionalDependencies": false,
+      "peerDependencies": false
+    }],
+    "react/prop-types": "off"
+  },
+};
---
+++ space-app/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 120,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  useTabs: true,
+  semi: true,
+  bracketSpacing: true,
+};
