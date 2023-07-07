90cc838f1ba805d2c3be78a62963f86158d148f6
vaniasuveev <vania-suveev@mail.ru>
2023-07-07 03:09:40 +0300

    refactor: one more fix

--- space-app/src/shared/button/ui/index.js
+++ space-app/src/shared/button/ui/index.js
@@ -1,5 +1,5 @@
 import styles from "./button.module.scss"
 
-export const Button = ({variant, text, onClick}) => (
+export const Button = ({ variant, text, onClick }) => (
   <button type="button" onClick={onClick} className={styles.Button} data-variant={variant}>{text}</button>
 )
